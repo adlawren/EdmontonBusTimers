7b691dcbe6cdc6f7490d3b312c7a87c988a052b1
TheELewis <enlewis@ualberta.ca>
2017-01-29 02:14:56 -0700

    Temp UI implementation for purposes of EditText testing.

--- MyApplication/app/src/main/java/firsttimesthecharm/edmontonbustimers/newBus_Activity.java
+++ MyApplication/app/src/main/java/firsttimesthecharm/edmontonbustimers/newBus_Activity.java
@@ -5,12 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class newBus_Activity extends AppCompatActivity {
 
     private Button commitBus;
+    private EditText userBus;
+    private int busNumber;
+    private int stopNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,7 @@ public class newBus_Activity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         commitBus = (Button)findViewById(R.id.button_commitBus);
+        userBus = (EditText)findViewById(R.id.editText_userBus);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +44,17 @@ public class newBus_Activity extends AppCompatActivity {
 
             }
         });
-    }
+        userBus.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionID, KeyEvent keyEvent) {
+                boolean handled = false;
+                if (actionID == EditorInfo.IME_ACTION_DONE) {
+                    busNumber = Integer.valueOf(userBus.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
 
 }
