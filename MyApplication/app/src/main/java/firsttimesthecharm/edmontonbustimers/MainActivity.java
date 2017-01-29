b4f189f0382e5342161da94ddcb4d7d7c9f54878
Robin <verleun@ualberta.ca>
2017-01-28 19:14:39 -0700

    Testing activities done

--- MyApplication/app/src/main/java/firsttimesthecharm/edmontonbustimers/MainActivity.java
+++ MyApplication/app/src/main/java/firsttimesthecharm/edmontonbustimers/MainActivity.java
@@ -1,5 +1,6 @@
 package firsttimesthecharm.edmontonbustimers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button jsonTest;
+    private Button backendTest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +24,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        jsonTest = (Button)findViewById(R.id.button_jsonTest);
+        backendTest = (Button)findViewById(R.id.button_backendTest);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        jsonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launch_jsonTest();
+            }
+        });
+
+        backendTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launch_backend();
             }
         });
     }
@@ -48,5 +71,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void launch_jsonTest() {
+        Intent intent = new Intent(this, jsonTestingActivity.class);
+        startActivity(intent);
+    }
+
+    private void launch_backend() {
+        Intent intent = new Intent(this, backendActivity.class);
+        startActivity(intent);
     }
 }
